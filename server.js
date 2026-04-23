c927bb5e876492536f03ad415e6de9e8490f2ea1
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-23 14:35:29 +0530

    Add MongoDB connection setup and error handling in server.js

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const express = require("express")
+require("dotenv").config();
+const mongoose = require("mongoose")
+
+if(!process.env.MONGO_URI){
+    console.log("There is no MONGO_URI")
+}
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("DB Connection successful"))
+.catch(err => console.log("Error in connection to DB", err))
+
 
 const app = express()
 
