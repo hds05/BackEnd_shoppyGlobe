087de7efb745fc0d86887e26c5320ec0bfdeb4ea
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-25 20:53:10 +0530

    Add User model and update Cart model to enforce required fields for validation

--- models/Cart.js
+++ models/Cart.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose")
 const cartSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
+        required: true
     },
     product: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product'
+        ref: 'Product',
+        required: true
     },
-    quantity: Number
+    quantity: {
+        type: Number,
+        default: 1,
+        min: 1
+    }
 })
 
 module.exports = mongoose.model('Cart', cartSchema)
